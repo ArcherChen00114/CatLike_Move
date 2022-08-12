dff275b4af267f927023ffb2510b4d3b9b9e2665
ArcherChen00114 <1034900424@qq.com>
2022-08-13 03:10:12 +0800

    3 jump

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -1,120 +1,276 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)]
-    float maxSpeed = 10f;
-    [SerializeField, Range(0f, 100f)]
-    float maxAcceleration = 10f, maxAirAcceleration = 1f;
-    [SerializeField, Range(0f, 0.2f)]
-    float fixedDeltaTime = 0.02f;
-    [SerializeField, Range(0f, 10f)]
-    float jumpHeight = 2f;
-    [SerializeField, Range(0, 5)]
-    int maxAirJumps = 0;
-    [SerializeField, Range(0f, 90f)]
-    float maxGroundAngle = 25f;
 
-    Vector3 velocity, desiredVelocity;
-    Rigidbody body;
-    bool desiredJump;
-    bool onGround;
-    int jumpPhase;
-    float minGroundDotProduct;
-    void OnValidate()
-    {
-        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
-    }
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        body = GetComponent<Rigidbody>();
-        Time.fixedDeltaTime = fixedDeltaTime;
+	[SerializeField, Range(0f, 100f)]
+	float maxSpeed = 10f;
 
-        OnValidate();
+	[SerializeField, Range(0f, 100f)]
+	float maxAcceleration = 10f, maxAirAcceleration = 1f;
 
-    }
-    void Start()
-    {
-        
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        //onGround = true;
-        EvaluateCollision(collision);
-    }
+	[SerializeField, Range(0f, 10f)]
+	float jumpHeight = 2f;
 
-    void OnCollisionStay(Collision collision)
-    {
-        //onGround = true;
-        EvaluateCollision(collision);
-    }
-    void EvaluateCollision(Collision collision)
-    {
-        for (int i = 0; i < collision.contactCount; i++)
-        {
-            Vector3 normal = collision.GetContact(i).normal;
+	[SerializeField, Range(0, 5)]
+	int maxAirJumps = 0;
 
-            onGround |= normal.y >= minGroundDotProduct;
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 playerInput;
-        playerInput.x = Input.GetAxis("Horizontal");
-        playerInput.y = Input.GetAxis("Vertical");
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+	//阶梯允许的坡度应当与斜坡不同
+	[SerializeField, Range(0, 90)]
+	float maxGroundAngle = 25f, maxStairsAngle = 50f;
 
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+	[SerializeField, Range(0f, 100f)]
+	float maxSnapSpeed = 100f;
 
+	[SerializeField, Min(0f)]
+	float probeDistance = 1f;
 
-        desiredJump |= Input.GetButtonDown("Jump");
+	[SerializeField]
+	LayerMask probeMask = -1, stairsMask = -1;
 
+	Rigidbody body;
 
-    }
-    private void FixedUpdate()
-    {
+	Vector3 velocity, desiredVelocity;
 
-        UpdateState();
-        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-        float maxSpeedChange = acceleration * Time.deltaTime;
-        velocity.x =
-            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z =
-            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        if (desiredJump)
-        {
-            desiredJump = false;
-            Jump();
-        }
-        body.velocity = velocity;
+	//SteepNormal用于计算垂直平面
+	Vector3 contactNormal, steepNormal;
 
-        onGround = false;
-    }
-    void UpdateState()
-    {
-        velocity = body.velocity;
-        if (onGround)
-        {
-            jumpPhase = 0;
-        }
-    }
-    void Jump()
-    {
-        if (onGround || jumpPhase < maxAirJumps)
-        {
-            jumpPhase += 1;
-            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-            if (velocity.y > 0f)
-            {
-                jumpSpeed = jumpSpeed - velocity.y;
-            }
-            velocity.y += jumpSpeed;
-        }
-    }
-	
+	bool desiredJump;
 
+	int groundContactCount, steepContactCount;
+
+	bool OnGround => groundContactCount > 0;
+
+	bool OnSteep => steepContactCount > 0;
+
+	int jumpPhase;
+
+	int stepsSinceLastGrounded, stepsSinceLastJump;
+
+	float minGroundDotProduct, minStairsDotProduct;
+
+	void OnValidate()
+	{
+		minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+		minStairsDotProduct = Mathf.Cos(maxStairsAngle * Mathf.Deg2Rad);
+	}
+
+	void Awake()
+	{
+		body = GetComponent<Rigidbody>();
+		OnValidate();
+	}
+
+	void Update()
+	{
+		Vector2 playerInput;
+		playerInput.x = Input.GetAxis("Horizontal");
+		playerInput.y = Input.GetAxis("Vertical");
+		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+		desiredVelocity =
+			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+		desiredJump |= Input.GetButtonDown("Jump");
+
+		GetComponent<Renderer>().material.SetColor(
+			"_Color", OnGround ? Color.black : Color.white
+		);
+	}
+
+	void FixedUpdate()
+	{
+		UpdateState();
+		AdjustVelocity();
+
+		if (desiredJump)
+		{
+			desiredJump = false;
+			Jump();
+		}
+
+		body.velocity = velocity;
+		ClearState();
+	}
+
+	void ClearState()
+	{
+		groundContactCount = steepContactCount = 0;
+		contactNormal = steepNormal = Vector3.zero;
+	}
+
+	void UpdateState()
+	{
+
+		stepsSinceLastGrounded += 1;
+		stepsSinceLastJump += 1;
+		velocity = body.velocity;
+		if (OnGround || SnapToGround() || CheckSteepContacts())
+		{
+			stepsSinceLastGrounded = 0;
+			jumpPhase = 0;
+			if (groundContactCount > 1)
+			{
+				contactNormal.Normalize();
+			}
+		}
+		else
+		{
+			contactNormal = Vector3.up;
+		}
+	}
+	bool SnapToGround()
+	{
+		//几个steps不处于地面上
+		//并且跳跃2steps不进行速度压制
+		if (stepsSinceLastGrounded > 1 || stepsSinceLastJump <= 2)
+		{
+			return false;
+		}
+		//检测速度，大于某个速度脱离压制起飞
+		float speed = velocity.magnitude;
+		if (speed > maxSnapSpeed)
+		{
+			return false;
+		}
+		//射线检测下方是否为地面，检测法线是否算作地面，也就是是否超过设定的可移动地面最大角度
+		if (!Physics.Raycast(body.position, Vector3.down,
+			out RaycastHit hit, probeDistance, probeMask))
+		{
+			return false;
+		}
+		if (hit.normal.y < GetMinDot(hit.collider.gameObject.layer))
+		{
+			return false;
+		}
+		//通过以上判定确定处于地面上，但是可能短暂离开地面
+		groundContactCount = 1;
+		contactNormal = hit.normal;
+
+		//speed获取速度的单位，dot点乘判断速读相对法线的面向
+		//速度减去面向，将物体的速度压向地面，归一化之后*速度返回原来的速度
+
+		float dot = Vector3.Dot(velocity, hit.normal);
+		//dot如果小于0，重新调整速度反而会减慢向地面收敛的速度，因此仅在>0再收敛
+		if (dot > 0f)
+		{
+			velocity = (velocity - hit.normal * dot).normalized * speed;
+		}
+		return true;
+	}
+	void AdjustVelocity()
+	{
+		Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+		Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+		float currentX = Vector3.Dot(velocity, xAxis);
+		float currentZ = Vector3.Dot(velocity, zAxis);
+
+		float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+		float maxSpeedChange = acceleration * Time.deltaTime;
+
+		float newX =
+			Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+		float newZ =
+			Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+		velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+	}
+	//检查该层是否计算碰撞，每个层获取一个掩码，如果不等于那就不为0
+	//不相等返回最小地面点积，如果相等那么返回楼梯点积
+	float GetMinDot(int layer)
+	{
+		return (stairsMask & (1 << layer)) == 0 ?
+			minGroundDotProduct : minStairsDotProduct;
+	}
+	void Jump()
+	{
+		Vector3 jumpDirection;
+		if (OnGround)
+		{
+			jumpDirection = contactNormal;
+		}
+		else if (OnSteep)
+		{
+			jumpDirection = steepNormal;
+			jumpPhase = 0;
+		}
+		else if (maxAirJumps > 0 && jumpPhase <= maxAirJumps)
+		{
+			if (jumpPhase == 0)
+			{
+				jumpPhase = 1;
+			}
+			jumpDirection = contactNormal;
+		}
+		else
+		{
+			return;
+		}
+		stepsSinceLastJump = 0;
+		if (stepsSinceLastJump > 1)
+		{
+			jumpPhase = 0;
+		}
+		jumpPhase += 1;
+			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+			jumpDirection = (jumpDirection + Vector3.up).normalized;
+			float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
+			if (alignedSpeed > 0f)
+			{
+				jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+			}
+			velocity += jumpDirection * jumpSpeed;
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void EvaluateCollision(Collision collision)
+	{
+		float minDot = GetMinDot(collision.gameObject.layer);
+		for (int i = 0; i < collision.contactCount; i++)
+		{
+			Vector3 normal = collision.GetContact(i).normal;
+			if (normal.y >= minDot)
+			{
+				groundContactCount += 1;
+				contactNormal += normal;
+			}
+			//检查有无垂直的接触面，用于防止卡在某些裂缝中
+			else if (normal.y > -0.01f)
+			{
+				steepContactCount += 1;
+				steepNormal += normal;
+			}
+		}
+	}
+	//检查是否有多个大于地面判定的接触面，如果有则返回true视为虚拟地面，允许球体跳跃
+	bool CheckSteepContacts()
+	{
+		if (steepContactCount > 1)
+		{
+			steepNormal.Normalize();
+			if (steepNormal.y >= minGroundDotProduct)
+			{
+				groundContactCount = 1;
+				contactNormal = steepNormal;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Vector3 ProjectOnContactPlane(Vector3 vector)
+	{
+		return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+	}
 }
